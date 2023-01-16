7801c1d311391277e0c89278109608906a16851d
Consultant <jaafarwerdi87@gmail.com>
2023-01-16 15:12:40 -0500

    First commit on remote

---
+++ countries/countriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  countriesApp.swift
+//  countries
+//
+//  Created by Consultant on 1/16/23.
+//
+
+import SwiftUI
+
+@main
+struct countriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
